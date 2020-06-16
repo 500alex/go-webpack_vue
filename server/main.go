7a4a8030b4b66d2ac96eb16350995fa867e7bb2d
500alex <7259606@mail.ru>
2020-06-16 21:36:41 +0300

    add webpack config and build

---
+++ server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("../templates/index.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	fmt.Println("get index")
+	t.ExecuteTemplate(w, "index", nil)
+}
+func main() {
+	fmt.Println("listening on port: 3000")
+	http.Handle("/build/", http.StripPrefix("/build/", http.FileServer(http.Dir("../build/"))))
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":3000", nil)
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// Webpack v4
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const compiler = require('vue-template-compiler')
+
+module.exports = {
+    entry: { main: './assets/js/main.js' },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'build.js'
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract(
+                    {
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'sass-loader']
+                    })
+            },
+
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("./build/build.css"),
+        new VueLoaderPlugin(),
+    ]
+};
